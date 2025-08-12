1f639d4e505cdb93417292508b6ce463a5f9f57e
stark <stark@stark>
2025-08-12 22:34:50 +0200

    finished quote porject and cleand up some code for it and added it to portfolio

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -4,21 +4,35 @@ const authorText = document.getElementById('author');
 const authorShow = document.getElementById('show');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+function spinnerLoading() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+}
+
+function spinnerComplete() {
+	quoteContainer.hidden = false;
+	loader.hidden = true
+}
+
 function newQuote() {
+	spinnerLoading();
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
 	authorText.textContent = quote.character;
 	quoteText.textContent = quote.quote;
 	authorShow.textContent = quote.show;
+	spinnerComplete();
 
 	console.log(quote)
 }
 
 // quotes from 
 async function getQuotes() {
+	spinnerLoading();
 	const apiUrl = 'https://yurippe.vercel.app/api/quotes';
 	try {
 		const response = await fetch(apiUrl);
